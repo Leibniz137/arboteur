bef8a74e50c29226cb793c6aec7121f4f462a61e
Leibniz137 <ncg09@hampshire.edu>
2019-03-18 15:36:43 -0700

    add uniprice.py

--- oracle.py
+++ oracle.py
@@ -1,17 +1,40 @@
+import statistics
+
 import requests
 
 
 def coinbase_price():
-    response = requests.get('https://api.coinbase.com/v2/')
+    url = 'https://api.coinbase.com/v2/exchange-rates?currency=eth'
+    response = requests.get(url)
     response.raise_for_status()
-    return response
+    return response.json()['data']['rates']['USDC']
+
 
 def etherscan_price():
-    pass
+    with open('./etherscan-api-token.txt') as fp:
+        api_token = fp.read().strip()
+    url = f'https://api.etherscan.io/api?module=stats&action=ethprice&apikey={api_token}'   # noqa: E501
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()['result']['ethusd']
+
+
+def coinmarketcap_price():
+    url = 'https://api.coinmarketcap.com/v1/ticker/ethereum'
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()[0]['price_usd']
 
 
 def main():
-    print(coinbase_price())
+    return statistics.median(
+        map(float, [
+            coinbase_price(),
+            etherscan_price(),
+            coinmarketcap_price()
+        ])
+    )
+
 
 if __name__ == '__main__':
-  main()
+    print(main())
---
+++ uniprice.py
@@ -0,0 +1,24 @@
+"""
+NOTE: requires Infura api key
+
+$ export WEB3_INFURA_API_KEY='...'
+"""
+from web3 import Web3
+
+
+DAI_EXCHANGE = '0x09cabec1ead1c0ba254b09efb3ee13841712be14'
+FACTORY = '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95'
+
+PROJECT_ID = '029890c5690243edbcb9fc201eb85164'
+PROVIDER_URL = f'https://mainnet.infura.io/v3/{PROJECT_ID}'
+
+
+def main():
+    provider = Web3.HTTPProvider(PROVIDER_URL)
+    w3 = Web3(provider)
+    assert w3.isConnected()
+    return w3.eth.blockNumber
+
+
+if __name__ == '__main__':
+    print(main())
