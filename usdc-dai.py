873377897e6d32fdbed193ae4f0fdbe764f707a1
Leibniz137 <ncg09@hampshire.edu>
2019-04-09 15:42:20 -0700

    use etherscan data to find tokenToToken transfer txs

--- usdc-dai.py
+++ usdc-dai.py
@@ -11,9 +11,7 @@ transfer:
 
 assumption: we don't need to consider private contract methods
 """
-import shlex
-import subprocess
-import tempfile
+import pathlib
 
 import pandas as pd
 
@@ -24,30 +22,25 @@ import uniswap
 LATEST_BLOCK = 7200000
 PROVIDER_URI = 'https://mainnet.infura.io'
 
+
+def extract_input(exchange, txhash):
+    tx = exchange.conn.w3.eth.getTransaction(txhash)
+    input_ = exchange.contract.decode_function_input(tx.input)
+    return input_
+
+
 if __name__ == '__main__':
     conn = web3_infura.Connection()
     exchange = uniswap.Exchange(uniswap.USDC_EXCHANGE_ADDR, conn)
     events = exchange.contract.events
     init_block = uniswap.USDC_MARKET_CREATION_BLOCK
-    cmd = shlex.split(
-        "ethereumetl export_blocks_and_transactions"
-        f" --start-block {uniswap.USDC_MARKET_CREATION_BLOCK}"
-        f" --end-block {LATEST_BLOCK}"
-        f" --provider-uri {PROVIDER_URI}"
-        " --transactions-output -"
-    )
 
-    completed_proc = subprocess.run(
-        cmd,
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        # check=True,
-    )
-    with tempfile.TemporaryFile() as fp:
-        fp.write(completed_proc.stdout)
-        fp.seek(0)
-        df = pd.read_csv(fp)
-    txs = df.loc[(df['to_address'] == uniswap.USDC_EXCHANGE_ADDR) | (df['from_address'] == uniswap.USDC_EXCHANGE_ADDR)]   # noqa: E501
+    records_path = pathlib.Path(__file__).parent / 'export-0x97dec872013f6b5fb443861090ad931542878126-jan1st.csv'   # noqa: E501
+    records = pd.read_csv(records_path)
+    inputs = [extract_input(exchange, txhash) for txhash in records.Txhash.to_dict()]   # noqa: E501
+    token_to_token_transfers = [
+        input_ for input_ in inputs if input_[0].fn_name.startswith('tokenToToken')   # noqa: E501
+    ]
     import pdb; pdb.set_trace()
 
     # for event in events.tokenToTokenSwapOutput(fromBlock=init_block):
